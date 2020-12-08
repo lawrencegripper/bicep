a081af1a35a19553911752b3d4f837e89730ddb2
Marcin Jastrzebski <majastrz@microsoft.com>
2020-12-07 16:02:27 -0800

    unfinished

---
+++ src/Bicep.LangServer/Handlers/BicepSignatureHelpHandler.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bicep.LanguageServer.Utils;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Bicep.LanguageServer.Handlers
+{
+    public class BicepSignatureHelpHandler: SignatureHelpHandler
+    {
+        public BicepSignatureHelpHandler() : base(CreateRegistrationOptions())
+        {
+        }
+
+        public override Task<SignatureHelp?> Handle(SignatureHelpParams request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<SignatureHelp?>(new SignatureHelp
+            {
+                Signatures = new Container<SignatureInformation>(
+                    new SignatureInformation
+                    {
+                        Label = "foo",
+                        Documentation = new StringOrMarkupContent(new MarkupContent
+                        {
+                            Kind = MarkupKind.Markdown,
+                            Value = "Foos the `foo` on the other `foo`."
+                        }),
+                        Parameters = new Container<ParameterInformation>(
+                            new ParameterInformation
+                            {
+                                Label = new ParameterInformationLabel("one"),
+                                Documentation = new StringOrMarkupContent(new MarkupContent
+                                {
+                                    Kind = MarkupKind.Markdown,
+                                    Value = "The first `foo`"
+                                })
+                            })
+                    })
+            });
+        }
+
+        private static SignatureHelpRegistrationOptions CreateRegistrationOptions() => new SignatureHelpRegistrationOptions
+        {
+            DocumentSelector = DocumentSelectorFactory.Create(),
+            TriggerCharacters = new Container<string>("("),
+            RetriggerCharacters = new Container<string>(",")
+        };
+    }
+}
